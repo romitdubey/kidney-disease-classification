38dc29bb3bb577e3110b11b26c71eea855f6a8ac
Romit Dubey <romitdubey1@gmail.com>
2023-12-26 16:34:58 +0530

    prepare base model edit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipline
+from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipline
 from cnnClassifier import logger
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -7,6 +8,18 @@ try:
     obj = DataIngestionTrainingPipline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<<\n\nx==================x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Prepare Base Model"
+try:
+    logger.info(f"**************************")
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<<")
+    obj = PrepareBaseModelTrainingPipline()
+    obj.main()
+    logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<")
 except Exception as e:
     logger.exception(e)
     raise e
--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -7,3 +7,14 @@ class DataIngestionConfig:
     source_URL: str
     local_data_file: Path
     unzip_dir: Path
+
+@dataclass(frozen=True)
+class PrepareBaseModelConfig:
+    root_dir: Path
+    base_model_path: Path
+    updated_base_model: Path
+    params_image_size: list
+    params_learning_rate: float
+    params_include_top: bool
+    params_weight: str
+    params_classes: int
