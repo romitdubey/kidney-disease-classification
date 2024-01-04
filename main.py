f8af7528bb08e9b4ce042ae52cb4a37e6c938aa4
Romit Dubey <romitdubey1@gmail.com>
2024-01-04 14:16:31 +0530

    model training

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipline
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipline
+from cnnClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
 from cnnClassifier import logger
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -18,6 +19,17 @@ try:
     logger.info(f"**************************")
     logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<<")
     obj = PrepareBaseModelTrainingPipline()
+    obj.main()
+    logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Training"
+try:
+    logger.info(f"**************************")
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<<")
+    obj = ModelTrainingPipeline()
     obj.main()
     logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<")
 except Exception as e:
--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -18,3 +18,14 @@ class PrepareBaseModelConfig:
     params_include_top: bool
     params_weight: str
     params_classes: int
+
+@dataclass
+class TrainingConfig:
+    root_dir: Path
+    trained_model_path: Path
+    updated_base_model_path: Path
+    training_data: Path
+    params_epoch: int
+    params_batch_size: int
+    params_is_augmentation: bool
+    params_image_size: list
